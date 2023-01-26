b10fd1b08498b887d85c9807ca37e0b697785118
Sondre Fjellving <sondrefjellving@gmail.com>
2023-01-26 20:06:57 +0100

    Add functionality and HTML to project

---
+++ index.js
@@ -0,0 +1,34 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+let pwContainer1 = document.getElementById("pw-el1")
+let pwContainer2 = document.getElementById("pw-el2")
+let password1;
+let password2;
+
+
+function resetPasswords() {
+    password1 = ""
+    password2 = ""
+}
+
+function printPasswords() {
+    resetPasswords()
+    generatePasswords(15)
+    pwContainer1.textContent = password1
+    pwContainer2.textContent = password2
+}
+
+function generatePasswords(passwordLength) {
+    for (let i = 0; i < passwordLength; i++) {
+        password1 += getRandomCharacter()
+        password2 += getRandomCharacter()
+    }
+}
+
+function getRandomCharacter() {
+    return characters[getRandomNumber()]
+}
+
+function getRandomNumber() {
+    return Math.floor( Math.random()*characters.length )
+}
